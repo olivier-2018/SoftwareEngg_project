ea640835196ce71acca9aa6cc110cb6d30c3dbe3
Olivier-2018 <oli@example.com>
2021-12-30 12:42:28 +0100

    added soundfile in requirements.txt

--- src_api/__init__.py
+++ src_api/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 import os
 
 app = Flask(__name__, template_folder=os.path.join("..", "templates"))
-# app = Flask(__name__, template_folder=os.path.join("..","templates")
 
 from src_api import views
 
